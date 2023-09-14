74533e95244ec44fb3fb6ee1b61fd9fed9489876
FlorianJ <jordany.florian@gmail.com>
2023-09-14 17:09:12 +0200

    MSE

--- beers.py
+++ beers.py
@@ -2,6 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
@@ -36,31 +41,78 @@ columns_to_bin = ["Size(L)", "OG", "FG", "Color", "BoilSize"]
 for column in columns_to_bin:
     df[f"{column}_Binned"] = pd.qcut(df[column], q=5, labels=False, duplicates="drop")
 
-##### SCATTER #####
-# fig, ax = plt.subplots()
-# cm_bright = ListedColormap(["#FF4500", "#0000CC"])
-# ax.scatter(df[df.columns[4]], df[df.columns[9]], cmap=cm_bright, edgecolors="k")
-# fig.savefig("./rapport/ABV-BoilGravity.png")
-
-##### PREDICTION ABV #####
+##### EVALUATION DU MODELE (MSE) #####
 X, y = df["OG"].values.reshape(-1, 1), df["ABV"]
 X2 = df["OG"].values.reshape(-1, 1)
-
 linear = linear_model.LinearRegression(fit_intercept=False)
 
 linear.fit(X, y)
 
 predictions = linear.predict(X2)
 
-print(predictions)
+y = df["ABV"]
 
-plt.scatter(X2, predictions, edgecolors="k")
-plt.xlabel("Prédictions")
-plt.ylabel("Vraies valeurs (ABV)")
-plt.savefig("./predictionABV.png")
-plt.show()
+y_pred = predictions
 
-##### PREDICTION IBU #####
+mse = np.mean((y - y_pred) ** 2)
+
+print("Mean Squared Error (MSE) :", mse)
+
+##### PREDICTION NON LINEAIRE POLYNOMIAL #####    
+# X = df["BoilTime"].values.reshape(-1, 1)
+# y = df["ABV"]
+# degree = 2  
+# poly_features = PolynomialFeatures(degree=degree)
+# X_poly = poly_features.fit_transform(X.reshape(-1, 1))
+
+# model = LinearRegression()
+# model.fit(X_poly, y)
+
+# x_pred = np.linspace(min(X), max(X), 100) 
+# X_pred_poly = poly_features.transform(x_pred.reshape(-1, 1))
+# y_pred = model.predict(X_pred_poly)
+
+# plt.scatter(X, y, label='Données originales')
+# plt.plot(x_pred, y_pred, color='red', label='Régression polynomiale')
+# plt.xlabel('BoilTime')
+# plt.ylabel('ABV')
+# plt.legend()
+# plt.savefig("./predictionNLpolynomial.png")
+
+##### MATRICE DE CORRELATION #####
+# plt.matshow(df.corr())
+# plt.xticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14, rotation=45)
+# plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14)
+# cb = plt.colorbar()
+# cb.ax.tick_params(labelsize=14)
+# plt.title('Correlation Matrix', fontsize=16)
+# plt.show()
+
+##### SCATTER #####
+# fig, ax = plt.subplots()
+# cm_bright = ListedColormap(["#FF4500", "#0000CC"])
+# ax.scatter(df[df.columns[4]], df[df.columns[9]], cmap=cm_bright, edgecolors="k")
+# fig.savefig("./rapport/ABV-BoilGravity.png")
+
+##### PREDICTION LINEAIRE ABV #####
+# X, y = df["OG"].values.reshape(-1, 1), df["ABV"]
+# X2 = df["OG"].values.reshape(-1, 1)
+
+# linear = linear_model.LinearRegression(fit_intercept=False)
+
+# linear.fit(X, y)
+
+# predictions = linear.predict(X2)
+
+# print(predictions)
+
+# plt.scatter(X2, predictions, edgecolors="k")
+# plt.xlabel("Prédictions")
+# plt.ylabel("Vraies valeurs (ABV)")
+# plt.savefig("./predictionABV.png")
+# plt.show()
+
+##### PREDICTION LINEAIRE IBU #####
 # X, y = df["OG"].values.reshape(-1, 1), df["IBU"]
 # X2 = df["OG"].values.reshape(-1, 1)
 
@@ -77,12 +129,3 @@ plt.show()
 # plt.ylabel("Vraies valeurs (IBU)")
 # plt.savefig("./predictionIBU.png")
 # plt.show()
-
-
-plt.matshow(df.corr())
-plt.xticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14, rotation=45)
-plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=14)
-cb = plt.colorbar()
-cb.ax.tick_params(labelsize=14)
-plt.title('Correlation Matrix', fontsize=16)
-#plt.show()
